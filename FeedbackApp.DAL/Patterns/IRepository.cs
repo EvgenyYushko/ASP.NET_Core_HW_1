217935edf4c4beed8baea87467b1f2c8255fbad6
evgeny <jeka-krut@mail.ru>
2021-09-05 21:13:58 +0300

    Добавил интерфейсы и репощитории дляраблты с БД а так же класс обвертку для получения singleinstance контекста

---
+++ FeedbackApp.DAL/Patterns/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using FeedbackApp.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FeedbackApp.DAL.Patterns
+{
+    public interface IRepository<TEntity> where TEntity : BaseEntity
+    {
+        Task<TEntity> CreateAsync(TEntity item);
+        Task<TEntity> UpdateAsync(TEntity item);
+        Task DeleteAsync(int id);
+        IQueryable<TEntity> GetAll();
+        Task<TEntity> GetByIdAsync(int id);
+        Task Save();
+    }
+}
---
+++ FeedbackApp.DAL/Patterns/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using FeedbackApp.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace FeedbackApp.DAL.Patterns
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IRepository<Product> Products { get; }
+        IRepository<MediaFile> MediaFiles { get; }
+        IRepository<Feedback> Feedbacks { get; }
+        IRepository<Comment> Comments { get; }
+
+        Task SaveAsync();
+    }
+}
