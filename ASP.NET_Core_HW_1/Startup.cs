b55fc8d5380761429bb5d3dd5da66a4a0e148371
evgeny <jeka-krut@mail.ru>
2021-09-05 21:22:02 +0300

    настроил стартап, добавил необходимые нагеты

--- ASP.NET_Core_HW_1/Startup.cs
+++ ASP.NET_Core_HW_1/Startup.cs
@@ -1,15 +1,13 @@
+using FeedbackApp.BLL.Interfaces;
+using FeedbackApp.BLL.Services;
+using FeedbackApp.DAL;
+using FeedbackApp.DAL.Patterns;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ASP.NET_Core_HW_1
 {
@@ -25,7 +23,18 @@ namespace ASP.NET_Core_HW_1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // получаем строку подключения из файла конфигурации
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+            // добавляем контекст MobileContext в качестве сервиса в приложение
+            services.AddDbContext<ApDbContext>(options =>
+                options.UseSqlServer(connection));
             services.AddControllers();
+
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+            services.AddTransient<ICommentService, CommentService>();
+            services.AddTransient<IFeedbackService, FeedbackService>();
+            services.AddTransient<IProductService, ProductService>();
+            services.AddTransient<IMediaFileService, MediaFileService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
